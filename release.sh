7995ef63a40d9bd6ce645040a5ccf2fc5b2c2da9
Ancoron Luciferis <ancoron.luciferis@gmail.com>
2012-06-24 14:50:58 +0200

    Add release helper script

---
+++ release.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+VERSION="$1"
+
+error()
+{
+    echo "$1" >&2
+    exit 1
+}
+
+if [ -z "$VERSION" ]; then
+    error "Please provide a release version string"
+fi
+
+DEV_VERSION=$(mvn help:evaluate -Dexpression=project.version -N | sed -n '/^[0-9]\+.*-SNAPSHOT$/p')
+
+mvn release:prepare \
+    -DautoVersionSubmodules=true \
+    -DdevelopmentVersion=${DEV_VERSION} \
+    -DreleaseVersion=${VERSION} \
+    -Dtag=release/${VERSION} \
+    -DpreparationGoals="clean,install,verify" || error "Release preparation failed!"
+
+mvn release:perform || error "Release failed!"
