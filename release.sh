ae368e30af3c32fb3e3658e0cebd9d14dcf780a1
Ancoron Luciferis <ancoron.luciferis@gmail.com>
2012-11-28 22:03:22 +0100

    Enhance release.sh script

--- release.sh
+++ release.sh
@@ -4,12 +4,32 @@ VERSION="$1"
 
 error()
 {
-    echo "$1" >&2
+    echo -e "$1" >&2
     exit 1
 }
 
+getProjectVersionXPath()
+{
+    xpath -q -e '/project/version' pom.xml | sed -e 's/^<version>//' -e 's/<\/version>.*$//' -e 's/-SNAPSHOT//'
+}
+
+getProjectVersionMaven()
+{
+    mvn org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version 2>/dev/null | sed '/^\[/d'
+}
+
+getProjectVersion()
+{
+    [ -n "$(which xpath 2>/dev/null)" ] && getProjectVersionXPath || getProjectVersionMaven
+}
+
+getLastReleaseVersion()
+{
+    git log --grep="maven-release-plugin" pom.xml | sed -n 's/^.*maven-release-plugin.*prepare release release\///p' | head -1
+}
+
 if [ -z "$VERSION" ]; then
-    error "Please provide a release version string"
+    error "Please provide a release version string\n--> suggesting: \"$(getProjectVersion)\"\n--> last release: \"$(getLastReleaseVersion)\""
 fi
 
 DEV_VERSION=$(mvn help:evaluate -Dexpression=project.version -N | sed -n '/^[0-9]\+.*-SNAPSHOT$/p')
